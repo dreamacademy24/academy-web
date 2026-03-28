26df53edbcc8036fea07f122796f63bb118ca25e
may <deskor112@gmail.com>
2026-03-28 17:57:28 +0900

    썸머캠프 페이지 추가 (/summer)

---
+++ app/summer/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function SummerPage() {
+  return (
+    <iframe
+      src="/summer.html"
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+    />
+  )
+}
