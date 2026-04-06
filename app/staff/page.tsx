eef8fa6b50b1617e0d10e5170509efa2363e8d40
may <deskor112@gmail.com>
2026-04-06 15:29:53 +0900

    Revert "fix: team_manager5.html로 CDN 캐시 완전 우회 - SyntaxError 없는 버전"
    This reverts commit d577f5f04b179fe6ccbf438f6fec5dac24d994ed.

--- app/staff/page.tsx
+++ app/staff/page.tsx
@@ -19,7 +19,7 @@ function StaffIframe() {
     const rawUser = sp.get('user') || info?.staffId || ''
     const userId = rawUser.replace(/^admin-/, '')
     if (userId) {
-      setSrc('/team_manager5.html?user=' + encodeURIComponent(userId))
+      setSrc('/team_manager3.html?user=' + encodeURIComponent(userId))
     } else {
       window.location.href = '/admin'
     }
